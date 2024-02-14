47c0550522ac0ed204d0afa479a9b9ca4e019a4c
devfancy <fancy.junyongmoon@gmail.com>
2024-02-14 17:47:42 +0900

    refactor: 인증된 사용자만 로그아웃 처리되도록 AuthenticationPrincipal 어노테이션 적용

--- src/main/java/com/payhere/auth/presentation/AuthController.java
+++ src/main/java/com/payhere/auth/presentation/AuthController.java
@@ -2,7 +2,7 @@ package com.payhere.auth.presentation;
 
 
 import com.payhere.auth.application.AuthService;
-import com.payhere.auth.dto.AuthInfo;
+import com.payhere.auth.dto.LoginOwner;
 import com.payhere.auth.dto.request.LoginRequest;
 import com.payhere.auth.dto.response.AccessTokenResponse;
 import org.springframework.http.ResponseEntity;
@@ -22,14 +22,14 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AccessTokenResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-        AuthInfo authInfo = authService.login(loginRequest);
-        AccessTokenResponse authResponse = authService.generateAccessToken(authInfo);
+        LoginOwner loginOwner = authService.login(loginRequest);
+        AccessTokenResponse authResponse = authService.generateAccessToken(loginOwner);
         return ResponseEntity.ok(authResponse);
     }
 
     @GetMapping("/logout")
-    public ResponseEntity<Void> logout(AuthInfo authInfo) {
-        authService.deleteToken(authInfo.getId());
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal LoginOwner loginOwner) {
+        authService.deleteToken(loginOwner.getId());
         return ResponseEntity.noContent().build();
     }
 }
