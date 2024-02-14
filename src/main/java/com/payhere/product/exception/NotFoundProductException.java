7385884bf5b2a2fec0f9c0a5ce71e3a7d609d914
devfancy <fancy.junyongmoon@gmail.com>
2024-02-14 23:13:07 +0900

    feat: 존재하지 않는 상품에 대한 예외 처리를 위한 클래스 생성

---
+++ src/main/java/com/payhere/product/exception/NotFoundProductException.java
@@ -0,0 +1,10 @@
+package com.payhere.product.exception;
+
+public class NotFoundProductException extends RuntimeException {
+    public NotFoundProductException(final String message) {
+        super(message);
+    }
+    public NotFoundProductException() {
+        this("존재하지 않는 상품입니다.");
+    }
+}
