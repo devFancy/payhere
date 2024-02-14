c114d3aef7f246c36cc16a24ca514ced5ad21768
devfancy <fancy.junyongmoon@gmail.com>
2024-02-14 18:49:33 +0900

    feat: 표준화된 API 응답을 위한 ApiResponse 클래스 구현
    - 모든 API 엔드포인트에 일관된 응답 구조를 제공하기 위해 ApiResponse 클래스 구현

---
+++ src/main/java/com/payhere/global/ApiResponse.java
@@ -0,0 +1,39 @@
+package com.payhere.global;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse (HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, String message, T data) {
+        return new ApiResponse<>(httpStatus, message, data);
+    }
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return of(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return of(HttpStatus.OK, data);
+    }
+
+    public static<T> ApiResponse<T> created(T data) {
+        return of(HttpStatus.CREATED, data);
+    }
+
+    public static <T> ApiResponse<T> noContent() {
+        return new ApiResponse<>(HttpStatus.NO_CONTENT, HttpStatus.NO_CONTENT.getReasonPhrase(), null);
+    }
+}
