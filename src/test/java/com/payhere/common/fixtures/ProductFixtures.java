580b63f2f56475856b233cd58530a73d20011dfa
devfancy <fancy.junyongmoon@gmail.com>
2024-02-16 15:16:17 +0900

    test: ProductServiceTest를 위한 ProductFixtures 에서 예시 값 추가

--- src/test/java/com/payhere/common/fixtures/ProductFixtures.java
+++ src/test/java/com/payhere/common/fixtures/ProductFixtures.java
@@ -14,11 +14,21 @@ public class ProductFixtures {
     public static final int 상품_원가 = 500;
     public static final String 상품_이름 = "아메리카노";
     public static final String 상품_설명 = "장인이 직접 제조하는 아메리카노";
-    public static final String 상품_바코드 = "1234-5678";
+    public static final String 상품_바코드 = "1234-56789";
     public static final LocalDateTime 상품_만기일 = LocalDateTime.of(2024, 12, 31, 23, 59);
     public static final ProductSize 상품_크기 = ProductSize.SMALL;
 
-    public static Product 상품(final Owner owner) {
+    /* 상품 2: 카페라떼 */
+    public static final ProductCategory 상품_카테고리2 = ProductCategory.HANDMADE;
+    public static final int 상품_가격2 = 4000;
+    public static final int 상품_원가2 = 1000;
+    public static final String 상품_이름2 = "카페라떼";
+    public static final String 상품_설명2 = "장인이 직접 제조하는 카페라떼";
+    public static final String 상품_바코드2 = "9876-54321";
+    public static final LocalDateTime 상품_만기일2 = LocalDateTime.of(2024, 12, 31, 23, 59);
+    public static final ProductSize 상품_크기2 = ProductSize.LARGE;
+
+    public static Product 아메리카노(final Owner owner) {
         return Product.builder()
                 .owner(owner)
                 .productCategory(상품_카테고리)
@@ -29,6 +39,21 @@ public class ProductFixtures {
                 .barcode(상품_바코드)
                 .expirationDate(상품_만기일)
                 .productSize(상품_크기)
+                .build();
+
+    }
+
+    public static Product 카페라떼(final Owner owner) {
+        return Product.builder()
+                .owner(owner)
+                .productCategory(상품_카테고리2)
+                .price(상품_가격2)
+                .cost(상품_원가2)
+                .name(상품_이름2)
+                .description(상품_설명2)
+                .barcode(상품_바코드2)
+                .expirationDate(상품_만기일2)
+                .productSize(상품_크기2)
                 .build();
 
     }
