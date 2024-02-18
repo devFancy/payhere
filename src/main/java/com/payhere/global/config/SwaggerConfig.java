f6ff1dce1817f7e054025ea72e55b6b4a5aeca87
devfancy <fancy.junyongmoon@gmail.com>
2024-02-18 20:27:22 +0900

    feat: swagger 설정

---
+++ src/main/java/com/payhere/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.payhere.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(schemaRequirement))
+                .info(new Info().title("Payhere Backend Engineer Assginment API")
+                        .description("Payhere Backend Engineer Assginment API 명세서입니다.")
+                        .version("v1.0.0"));
+    }
+
+}
